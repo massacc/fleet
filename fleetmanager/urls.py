7d8e6a002da56e156fd993172c70a205e80134fe
Jan Kowalski <toja@example.com>
2022-06-13 22:15:38 +0200

    routers for serializers for vehicle added

--- fleetmanager/urls.py
+++ fleetmanager/urls.py
@@ -20,25 +20,16 @@ from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 from django.conf.urls.i18n import i18n_patterns
 
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'is_staff']
+from vehicle.views import VehicleViewSet
 
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'vehicles', VehicleViewSet, basename='vehicles')
 
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('rosetta/', include('rosetta.urls')),
     path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', include('vehicle.urls', namespace='vehicle')),
 )
