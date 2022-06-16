3e012689e5b56706b327ed8b0824cb65ead2895e
Jan Kowalski <toja@example.com>
2022-06-16 11:25:10 +0200

    DateInput set to date fields

--- vehicle/forms.py
+++ vehicle/forms.py
@@ -1,15 +1,24 @@
 from django import forms
 from .models import Vehicle, Registration
 
+
 class VehicleForm(forms.ModelForm):
     class Meta:
         model = Vehicle
         exclude = ('id',)
+        widgets = {'first_registration':forms.widgets.DateInput(attrs=  {'type':'date'})
+
+        }
 
 class RegistrationForm(forms.ModelForm):
     class Meta:
         model = Registration
         fields = ('plate', 'start_date', 'end_date')
+        widgets = {
+            'start_date':forms.widgets.DateInput(attrs={'type':'date'}),
+            'end_date':forms.widgets.DateInput(attrs={'type':'date'})
+        }
+        
 
 class VehicleDeleteConfirmForm(forms.Form):
         items = forms.CharField(disabled=True)
