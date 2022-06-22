56873cb809db380fff13264820a980a4e62cf852
Jan Kowalski <toja@example.com>
2022-06-22 23:47:53 +0200

    validation method for end_date field created

--- vehicle/forms.py
+++ vehicle/forms.py
@@ -1,6 +1,9 @@
+from xml.dom import ValidationErr
 from django import forms
+from traitlets import ValidateHandler
 from .models import Vehicle, Registration
-
+from datetime import date
+from django.utils.translation import gettext_lazy as _
 
 class VehicleForm(forms.ModelForm):
     class Meta:
@@ -11,9 +14,17 @@ class VehicleForm(forms.ModelForm):
         }
 
 class RegistrationForm(forms.ModelForm):
+    
+    def clean_end_date(self):
+        end_date = self.cleaned_data.get('end_date')
+        start_date = self.cleaned_data.get('start_date')
+        if end_date is not None and start_date is not None:
+            if end_date < start_date:
+                raise forms.ValidationError(_('Deregistration cannot be done before registration'))
+
     class Meta:
         model = Registration
-        fields = ('plate', 'start_date', 'end_date')
+        fields = ('plate', 'start_date', 'end_date', 'active')
         widgets = {
             'start_date':forms.widgets.DateInput(attrs={'type':'date'}),
             'end_date':forms.widgets.DateInput(attrs={'type':'date'})
